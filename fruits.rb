26e06eead94b258bda42d63bc42dbc311ddbcdeb
Brian <riceb53@gmail.com>
2019-02-18 19:26:37 -0600

    add file showing two different ways to loop

---
+++ fruits.rb
@@ -0,0 +1,15 @@
+# times loop
+
+# fruits = ['apple', 'banana', 'mango', 'kiwi']
+
+# i = 0
+# fruits.length.times do
+  # p fruits[i]
+  # i += 1
+# end
+
+fruits = ['apple', 'banana', 'mango', 'kiwi']
+
+fruits.each do |fruit|
+  p fruit
+end
